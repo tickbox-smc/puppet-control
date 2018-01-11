7fc66bf71f95b571e65a0ba0c48dea7caf7b9fff
root <root@puppet.tickbox>
2018-01-11 19:22:37 +0000

    typo

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -3,7 +3,7 @@ class profile::base {
   #the base profile should include component modules that will be on all nodes
   $password = lookup('profile::nginx::vhosts::password'),
 
-  notice {"password = ${password}":}
+  notice {"password = ${password}":},
   notify { 'This is profile::base':  }
 
 }
