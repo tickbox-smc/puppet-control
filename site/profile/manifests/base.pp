fc01df42e9ab4e774a1fb10a2e520deb85ac6c09
root <root@puppet.tickbox>
2018-01-11 19:08:58 +0000

    eyaml

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,8 +1,8 @@
 class profile::base {
 
-  #the base profile should include component modules that will be on all nodes
-  #$password = lookup('profile::nginx::vhosts::password'),
+  the base profile should include component modules that will be on all nodes
+  $password = lookup('profile::nginx::vhosts::password'),
 
-  #notice {"password = ${password}":}
+  notice {"password = ${password}":}
 
 }
