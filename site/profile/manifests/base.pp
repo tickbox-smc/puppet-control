8408cea63013df294f80f18b57ebdf7eb1c39c01
root <root@puppet.tickbox>
2018-01-11 19:25:28 +0000

    fix issues

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,9 +1,9 @@
 class profile::base {
 
   #the base profile should include component modules that will be on all nodes
-  $password = lookup('profile::nginx::vhosts::password'),
+  $password = lookup('profile::nginx::vhosts::password')
 
-  notice {"password = ${password}":},
+  notice { "password = ${password}": }
   notify { 'This is profile::base':  }
 
 }
